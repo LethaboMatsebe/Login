6cb5b2efe0b9e6afe9cb329564158bb79005fd2c
matsebelethabo <lethabomatsebe@gmail.com>
2026-07-07 09:22:06 +0200

    added hosted urls for registration

--- src/main/java/com/example/Login/interfaces/UserClient.java
+++ src/main/java/com/example/Login/interfaces/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "Demo",url ="http://localhost:8080")
+@FeignClient(name = "Demo",url ="https://demo-3-asug.onrender.com")
 public interface UserClient {
     @GetMapping("/api/users/email/{email}")
     UserResponse findByEmail(@PathVariable("email") String email);
